9d995f35ecc83453395435f8492ca7f1be907352
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2023-09-22 23:20:48 +0200

    Initial Commit

---
+++ MiBand5UITests/MiBand5UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MiBand5UITestsLaunchTests.swift
+//  MiBand5UITests
+//
+//  Created by Aitor Zubizarreta on 2023-09-22.
+//
+
+import XCTest
+
+final class MiBand5UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
